454f9cc81e5c164b11fa93eb3c623e491c084980
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2019-04-14 21:19:40 +0530

    two files merged into one

--- two_pass_macroprocessor/TwoPassMacroprocessor.java
+++ two_pass_macroprocessor/TwoPassMacroprocessor.java
@@ -54,7 +54,7 @@ class MDT{
     }
 }
 
-public class TwoPassAssembler {
+public class TwoPassMacroprocessor {
     static List<List<String []>> passes = new ArrayList<>();
     static List<List<String []>> non_passes = new ArrayList<>();
     static List<String> ALA = new ArrayList<>();
@@ -203,5 +203,35 @@ public class TwoPassAssembler {
 
         System.out.println("\n-------- PASS 2 --------\n");
         printTables();
+    }
+
+    public static void main(String[] args) {
+        String input_string = "START\n" +
+                "\n" +
+                "MACRO\n" +
+                "ADD &ARG1,&ARG2\n" +
+                "L 1,&ARG1\n" +
+                "A 1,&ARG2\n" +
+                "MEND\n" +
+                "\n" +
+                "MACRO\n" +
+                "SUB &ARG3,&ARG4\n" +
+                "L 1,&ARG3\n" +
+                "S 1,&ARG4\n" +
+                "MEND\n" +
+                "\n" +
+                "ADD DATA1,DATA2\n" +
+                "SUB DATA1,DATA2\n" +
+                "\n" +
+                "DATA1 DC F'9'\n" +
+                "DATA2 DC F'5'\n" +
+                "\n" +
+                "END";
+
+        List<List<String []>> input = parseInput(input_string);
+        generatePasses(input);
+        populateALA(passes);
+        performPass1();
+        performPass2();
     }
 }
